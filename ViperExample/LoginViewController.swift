ff6ccf76bafd85be88428b396150bf8a90d438a4
Yura Savchuk <yura.savchuk22@gmail.com>
2021-12-30 11:24:40 +0200

    First commit

---
+++ ViperExample/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  ViperExample
+//
+//  Created by Yura Savchuk on 30.12.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
